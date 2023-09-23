18bddfe99423bc9b77a6de7a13cf6d08f7e8a0f7
mihai <29628232+mihaiofficialRO@users.noreply.github.com>
2023-09-23 04:38:07 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,26 @@
-
+{
+        name: "Numele textului tau",
+        message: 'Salut !\n Acesta este textul meu predefinit.',
+        group_id: 0,
+        type: "bold"
+},
+var zModGroups = [{
+        id: 0,
+        name: 'Grup 1'
+},
+{
+        id: 1,
+        name: 'Grup 2'
+},
+{
+        id: 2,
+        name: 'Grup 3'
+},
+{
+        id: 3,
+        name: 'Grup 4'
+},
+{
+        id: 4,
+        name: 'Grup 5'
+}];
